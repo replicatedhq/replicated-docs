d31f2343a23a62437edd219bdd20b0f9b17afae0
Mia Wong <miawongdev@gmail.com>
2022-06-27 15:01:05 -0700

    Miawong/sc 50446/ability to link to the create new issue page (#426)
    * add feedback link
    
    * add feedback icon
    
    * add margin
    
    * update id and description

--- src/theme/EditThisPage/index.js
+++ src/theme/EditThisPage/index.js
@@ -4,23 +4,45 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
-import Translate from '@docusaurus/Translate';
-import IconEdit from '@theme/IconEdit';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-export default function EditThisPage({editUrl}) {
+import React from "react";
+import Translate from "@docusaurus/Translate";
+import IconEdit from "@theme/IconEdit";
+import FeedbackIcon from "../../../static/images/feedback-icon.svg";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+export default function EditThisPage({ editUrl }) {
   return (
-    <a
-      href={editUrl}
-      target="_blank"
-      rel="noreferrer noopener"
-      className={ThemeClassNames.common.editThisPage}>
-      <IconEdit />
-      <Translate
-        id="theme.common.editThisPage"
-        description="The link label to edit the current page">
-        Create pull request or raise issue on GitHub
-      </Translate>
-    </a>
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <a
+        href={editUrl}
+        target="_blank"
+        rel="noreferrer noopener"
+        className={ThemeClassNames.common.editThisPage}
+      >
+        <IconEdit />
+        <Translate
+          id="theme.common.editThisPage"
+          description="The link label to edit the current page"
+        >
+          Create pull request
+        </Translate>
+      </a>
+      <a
+        href={"https://github.com/replicatedhq/replicated-docs/issues/new"}
+        target="_blank"
+        rel="noreferrer noopener"
+        className={ThemeClassNames.common.editThisPage}
+        style={{ textAlign: "right" }}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <FeedbackIcon width="28" height="26" style={{ marginRight: "8px" }} />
+          <Translate
+            id="theme.common.provideFeedback"
+            description="The link label to provide feedback in github"
+          >
+            Provide feedback in GitHub
+          </Translate>
+        </div>
+      </a>
+    </div>
   );
 }
