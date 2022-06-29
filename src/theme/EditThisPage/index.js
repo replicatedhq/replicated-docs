abb6c42424e91b91d7772424b03a9dfb7baaf1d6
Mia Wong <miawongdev@gmail.com>
2022-06-29 09:56:29 -0700

    Update create a PR and issue Github icons (#430)
    * update to new icons
    add css file
    
    * remove icon edit
    
    * update wording
    add button styles
    
    * update buttons to be side by side
    
    * add cursor

--- src/theme/EditThisPage/index.js
+++ src/theme/EditThisPage/index.js
@@ -6,40 +6,45 @@
  */
 import React from "react";
 import Translate from "@docusaurus/Translate";
-import IconEdit from "@theme/IconEdit";
-import FeedbackIcon from "../../../static/images/feedback-icon.svg";
 import { ThemeClassNames } from "@docusaurus/theme-common";
+import ReportIcon from "../../../static/images/report.svg";
+import PullRequestIcon from "../../../static/images/git-pull-request.svg";
+import styles from "./styles.module.css";
+
 export default function EditThisPage({ editUrl }) {
   return (
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
+    <div className={styles.githubLinksWrapper}>
       <a
         href={editUrl}
         target="_blank"
         rel="noreferrer noopener"
         className={ThemeClassNames.common.editThisPage}
+        style={{ textDecoration: "none" }}
       >
-        <IconEdit />
-        <Translate
-          id="theme.common.editThisPage"
-          description="The link label to edit the current page"
-        >
-          Create pull request
-        </Translate>
+        <div className={styles.iconTextWrapper}>
+          <PullRequestIcon className={styles.icon} />
+          <Translate
+            id="theme.common.editThisPage"
+            description="The link label to edit the current page"
+          >
+            Propose Changes
+          </Translate>
+        </div>
       </a>
       <a
         href={"https://github.com/replicatedhq/replicated-docs/issues/new"}
         target="_blank"
         rel="noreferrer noopener"
         className={ThemeClassNames.common.editThisPage}
-        style={{ textAlign: "right" }}
+        style={{ textAlign: "right", textDecoration: "none" }}
       >
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <FeedbackIcon width="28" height="26" style={{ marginRight: "8px" }} />
+        <div className={styles.iconTextWrapper}>
+          <ReportIcon className={styles.icon} />
           <Translate
             id="theme.common.provideFeedback"
             description="The link label to provide feedback in github"
           >
-            Provide feedback in GitHub
+            Provide feedback
           </Translate>
         </div>
       </a>
