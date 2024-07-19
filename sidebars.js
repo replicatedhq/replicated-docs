e0b1669378bfcf5897b7432616f5e94d8b91301f
Paige Calvert <paige@replicated.com>
2024-07-19 12:19:48 -0600

    air gap docs

--- sidebars.js
+++ sidebars.js
@@ -222,6 +222,7 @@ const sidebars = {
                   items: [
                     'vendor/embedded-overview',
                     'reference/embedded-config',
+                    'vendor/embedded-air-gap',
                     'vendor/embedded-disaster-recovery',
                   ],
                 },
