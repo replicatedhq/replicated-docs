c85c3cad03e5a83159ce25d1d450b3425ade1b73
Paige Calvert <paige@replicated.com>
2024-09-13 11:53:13 -0600

    edit footer js

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import clsx from 'clsx';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-// import {useDoc} from '@docusaurus/theme-common/internal';
 import LastUpdated from '@theme/LastUpdated';
 import EditThisPage from '@theme/EditThisPage';
 import TagsListInline from '@theme/TagsListInline';
 import styles from './styles.module.css';
+import {ThemeClassNames} from '@docusaurus/theme-common';
+
 function TagsRow(props) {
   return (
     <div
@@ -19,6 +19,7 @@ function TagsRow(props) {
     </div>
   );
 }
+
 function EditMetaRow({
   editUrl,
   lastUpdatedAt,
@@ -26,7 +27,7 @@ function EditMetaRow({
   formattedLastUpdatedAt,
 }) {
   return (
-    <div className={clsx(ThemeClassNames.docs.docFooterEditMetaRow, 'row')}>
+    <div className={ThemeClassNames.docs.docFooterEditMetaRow}>
       <div className="col">{editUrl && <EditThisPage editUrl={editUrl} />}</div>
 
       <div className={clsx('col', styles.lastUpdated)}>
@@ -41,16 +42,20 @@ function EditMetaRow({
     </div>
   );
 }
-export default function DocItemFooter() {
-  // const {metadata} = useDoc();
+
+export default function DocItemFooter(props) {
+  const {content: DocContent} = props;
+  const {metadata} = DocContent;
   const {editUrl, lastUpdatedAt, formattedLastUpdatedAt, lastUpdatedBy, tags} =
     metadata;
   const canDisplayTagsRow = tags.length > 0;
   const canDisplayEditMetaRow = !!(editUrl || lastUpdatedAt || lastUpdatedBy);
   const canDisplayFooter = canDisplayTagsRow || canDisplayEditMetaRow;
+
   if (!canDisplayFooter) {
-    return null;
+    return <></>;
   }
+
   return (
     <footer
       className={clsx(ThemeClassNames.docs.docFooter, 'docusaurus-mt-lg')}>
@@ -66,3 +71,76 @@ export default function DocItemFooter() {
     </footer>
   );
 }
+
+
+
+
+// import React from 'react';
+// import clsx from 'clsx';
+// import {ThemeClassNames} from '@docusaurus/theme-common';
+// import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+// // import {useDoc} from '@docusaurus/theme-common/internal';
+// import LastUpdated from '@theme/LastUpdated';
+// import EditThisPage from '@theme/EditThisPage';
+// import TagsListInline from '@theme/TagsListInline';
+// import styles from './styles.module.css';
+// function TagsRow(props) {
+//   return (
+//     <div
+//       className={clsx(
+//         ThemeClassNames.docs.docFooterTagsRow,
+//         'row margin-bottom--sm',
+//       )}>
+//       <div className="col">
+//         <TagsListInline {...props} />
+//       </div>
+//     </div>
+//   );
+// }
+// function EditMetaRow({
+//   editUrl,
+//   lastUpdatedAt,
+//   lastUpdatedBy,
+//   formattedLastUpdatedAt,
+// }) {
+//   return (
+//     <div className={clsx(ThemeClassNames.docs.docFooterEditMetaRow, 'row')}>
+//       <div className="col">{editUrl && <EditThisPage editUrl={editUrl} />}</div>
+
+//       <div className={clsx('col', styles.lastUpdated)}>
+//         {(lastUpdatedAt || lastUpdatedBy) && (
+//           <LastUpdated
+//             lastUpdatedAt={lastUpdatedAt}
+//             formattedLastUpdatedAt={formattedLastUpdatedAt}
+//             lastUpdatedBy={lastUpdatedBy}
+//           />
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+// export default function DocItemFooter() {
+//   const {metadata} = useDocusaurusContext();
+//   const {editUrl, lastUpdatedAt, formattedLastUpdatedAt, lastUpdatedBy, tags} =
+//     metadata;
+//   const canDisplayTagsRow = tags.length > 0;
+//   const canDisplayEditMetaRow = !!(editUrl || lastUpdatedAt || lastUpdatedBy);
+//   const canDisplayFooter = canDisplayTagsRow || canDisplayEditMetaRow;
+//   if (!canDisplayFooter) {
+//     return null;
+//   }
+//   return (
+//     <footer
+//       className={clsx(ThemeClassNames.docs.docFooter, 'docusaurus-mt-lg')}>
+//       {canDisplayTagsRow && <TagsRow tags={tags} />}
+//       {canDisplayEditMetaRow && (
+//         <EditMetaRow
+//           editUrl={editUrl}
+//           lastUpdatedAt={lastUpdatedAt}
+//           lastUpdatedBy={lastUpdatedBy}
+//           formattedLastUpdatedAt={formattedLastUpdatedAt}
+//         />
+//       )}
+//     </footer>
+//   );
+// }
