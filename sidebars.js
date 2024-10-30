d78c550fdee5b21e5f512c1972d7f77bd84cf1f1
Paige Calvert <paige@replicated.com>
2024-10-30 10:26:16 -0600

    docs edits part 2

--- sidebars.js
+++ sidebars.js
@@ -494,6 +494,7 @@ const sidebars = {
       'vendor/helm-install-overview',
       'vendor/install-with-helm',
       'vendor/helm-install-airgap',
+      'vendor/helm-install-airgap-registry-proxy',
     ],
   },
   {
