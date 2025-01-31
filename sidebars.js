91f1fae74739af4254a5dab0d910e3d3752008d7
Paige Calvert <paige@replicated.com>
2025-01-31 11:53:13 -0700

    Add Helm troubleshooting info

--- sidebars.js
+++ sidebars.js
@@ -508,6 +508,7 @@ const sidebars = {
       'vendor/install-with-helm',
       'vendor/helm-install-airgap',
       'vendor/using-third-party-registry-proxy',
+      'vendor/helm-install-troubleshooting',
     ],
   },
   {
