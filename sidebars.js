21a2a5627fcae3d4ace140ddb46f6f5108b5bf66
Xav Paice <xav@replicated.com>
2026-07-02 15:32:11 +1200

    Add troubleshooting topic for reclaiming snapshot disk space

--- sidebars.js
+++ sidebars.js
@@ -408,6 +408,7 @@ const sidebars = {
             "enterprise/snapshots-restoring-full",
             "enterprise/snapshots-updating-with-admin-console",
             "enterprise/snapshots-troubleshooting-backup-restore",
+            "enterprise/snapshots-troubleshooting-reclaim-space",
             "enterprise/snapshots-velero-upgrading",
           ],
         },
